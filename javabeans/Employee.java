b7df261e4b0d12f365692a37b2daaeb9ad428dd3
mishrasunny174 <mishrasunny174@gmail.com>
2019-10-03 13:36:00 +0530

    added javabeans practicals

---
+++ javabeans/Employee.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Employee implements Serializable {
+    public static final long serialVersionUID = 2;
+    private String empName;
+    private int empid;
+    private double salary;
+    private Date birthday;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ javabeans/Student.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    public static final long serialVersionUID = 1;
+    private String name;
+    private int rollNumber;
+    private int age;
+    private String mobileNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    
+}
